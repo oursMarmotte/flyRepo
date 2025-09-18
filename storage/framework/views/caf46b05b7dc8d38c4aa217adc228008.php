acce33b19db7d86abed98a6882c544791ff9fd2f
oursMarmotte <jeangerald.centaure@gmail.com>
2025-09-18 20:47:01 +0200

    first commit

---
+++ storage/framework/views/caf46b05b7dc8d38c4aa217adc228008.php
@@ -0,0 +1,6 @@
+
+# See https://laravel.com/docs/9.x/octane#serving-your-application-via-nginx
+map $http_upgrade $connection_upgrade {
+    default upgrade;
+    ''      close;
+}<?php /**PATH phar://C:/Users/marmotte/Documents/monRestaurant_SERV/backend/vendor/fly-apps/dockerfile-laravel/builds/dockerfile-laravel\resources\views/fly/nginx/conf_d/websockets_conf.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f8e5bf4a6e2801ff2d540c3766569836.php
@@ -0,0 +1,5 @@
+
+#!/usr/bin/env bash
+
+sleep 0.25 && exec nginx
+<?php /**PATH phar://C:/Users/marmotte/Documents/monRestaurant_SERV/backend/vendor/fly-apps/dockerfile-laravel/builds/dockerfile-laravel\resources\views/fly/start-nginx_sh.blade.php ENDPATH**/ ?>
